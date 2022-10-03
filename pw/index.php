e260fc7ab72fdea92ebbc9d526eca7834741c203
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-10-03 13:26:34 +0700

    mengubah warna bg di index.php

--- pw/index.php
+++ pw/index.php
@@ -15,6 +15,7 @@ $buku = query("SELECT * FROM buku");
   <meta charset="utf-8">
   <title>Fikri's Library</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
