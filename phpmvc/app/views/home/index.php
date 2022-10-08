b36a5ea8bbfad6436f80cf82e039f423a6230540
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-10-08 18:47:36 +0700

    menyelesaikan video 7 : Model

--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -2,7 +2,7 @@
 
   <div class="container mt-4">
     <h1 class="display-4">Selamat Datang di Website Saya!</h1>
-    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque velit modi, veritatis placeat illum repellat cum beatae. Nesciunt quas sed velit aperiam, distinctio eaque ipsam non, soluta blanditiis cumque magni.</p>
+    <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
     <hr class="my-4">
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta necessitatibus delectus rem pariatur corrupti nobis deserunt ut, ipsa neque impedit architecto atque dolore voluptatem ipsum et aperiam, odit harum praesentium!</p>
     <a href="#" class="btn btn-primary btn-lg" role="button">Learn More.</a>
