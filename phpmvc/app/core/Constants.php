4466ac54ad610b27cf8bf7023cb3f63e410e1359
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-10-08 15:33:49 +0700

    menyelesaikan video 6 : Assets

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_a_203040001/phpmvc/public');
--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,1 +1,11 @@
-<h1>Selamat Datang di Website Saya!</h1>
+<div class="container">
+
+  <div class="container mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque velit modi, veritatis placeat illum repellat cum beatae. Nesciunt quas sed velit aperiam, distinctio eaque ipsam non, soluta blanditiis cumque magni.</p>
+    <hr class="my-4">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta necessitatibus delectus rem pariatur corrupti nobis deserunt ut, ipsa neque impedit architecto atque dolore voluptatem ipsum et aperiam, odit harum praesentium!</p>
+    <a href="#" class="btn btn-primary btn-lg" role="button">Learn More.</a>
+  </div>
+
+</div>
